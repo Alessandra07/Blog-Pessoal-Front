939dac60d64a8dcaf88904e6f5fb70fceea6e9e7
Alessandra Silva <alessandra.silva070996@gmail.com>
2022-05-03 19:00:17 -0300

    Fazendo a rota da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
-import './App.css';
 import Home from './paginas/Home/Home';
+import './App.css';
+
 
 function App() {
-  return (
-    <>
-    <Navbar/>
-    < Home />
-    <Footer/>
-
-    </>
-
-  );
+  return(
+    <Router>
+    <Navbar />
+    <div style={{ minHeight: '100vh' }}>
+    <Routes> // Antigo Switch
+    <Route path="/home" element={<Home />} />
+    </Routes>
+    </div>
+    <Footer />
+    </Router>
+    )
+    
 }
 
 export default App;
