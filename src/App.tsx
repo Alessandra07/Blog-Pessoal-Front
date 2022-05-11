019e87979589a54996acb2b3c1f866cde301f880
Alessandra Silva <alessandra.silva070996@gmail.com>
2022-05-11 19:40:39 -0300

    Temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/Home/Home';
-import Login from './paginas/Login/Login';
-import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import './App.css';
-
+import Login from './paginas/Login/Login'
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listaTemas/ListaTema'
+import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem'
 
 function App() {
-  return(
+  return (
     <Router>
-    <Navbar />
-    <div style={{ minHeight: '100vh' }}>
-    <Routes> // Antigo Switch
-    <Route path="/" element={<Login />} />
+      <Navbar />
+      <Routes>
 
-<Route path="/home" element={<Home />} />
+        <Route path="/" element={<Login  />} />
 
-<Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
 
-<Route path="/cadastrousuario" element={<CadastroUsuario />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+
+        <Route path="/tema" element={<ListaTema />} />
+
+        <Route path="/postagem" element={<ListaPostagem />} />
 
 
-    
-    </Routes>
-    </div>
-    <Footer />
+      </Routes>
+      <Footer />
     </Router>
-    )
-    
 
+  );
 }
 
 export default App;
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -5,45 +5,43 @@ import './Navbar.css'
 function Navbar() {
     return (
         <>
-            <AppBar position="static"  >
-                <Toolbar variant="dense" >
-                    <Link to='/Home'>
-                        <Box className='cursor'>
-                            <Typography variant="h5" color="inherit">
-                                Home
-                            </Typography>
-                        </Box>
-                    </Link>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            BlogPessoal
+                        </Typography>
+                    </Box>
 
                     <Box display="flex" justifyContent="start">
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                Quem somos
+                                home
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                O Espaço
+                                postagens
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                Mural
+                                temas
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit" >
-                                Agendamentos
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
                             </Typography>
                         </Box>
                         <Link to='/login' className='text-decorator-none'>
                             <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit" >
-                                    Login
+                                <Typography variant="h6" color="inherit">
+                                    logout
                                 </Typography>
                             </Box>
                         </Link>
-
+                        
                     </Box>
 
                 </Toolbar>
